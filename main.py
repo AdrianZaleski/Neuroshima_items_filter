827497d448c535775f261d0910524ad44d786046
Adrian <azuniasty@gmail.com>
2021-09-16 20:29:03 +0200

    Implementing pandas for CSV file import - take 1

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 """
-Główny plik do obsługi sortowań
-TODO - rozpisać tutaj wszystko
-    Wkleić tak samo zawartości odpowiednie
-    Zapewne będzie to zrobione na sam koniec dopiero aby spiąć to jakoś.
+Making a database from CSV file - take 1
 """
 
-if __name__ == '__main__':
+import pandas as pd
 
-#   TODO: Wrzucić tutaj obsługę pod programów do jednego miejsca. Aby było to prawdziwy main.
+if __name__ == "__main__":
+    data = pd.read_csv(r"CSV/Bronie prototyp - RANGED.csv")
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "ID",
+            "Name",
+            "Weapon Class",
+            "Ammo_id",
+            "Magazine",
+            "Misfire roll",
+            "Celność w %",
+            "Zasięg [m]",
+            "Fire Rate",
+            "Attack Type",
+            "Reload",
+            "Punkty przebicia",
+            "Damage",
+            "Strength",
+            "Holster",
+            "Price",
+            "Availability",
+            "Actions",
+            "Description",
+        ]
+    )
+    print(df)
+
