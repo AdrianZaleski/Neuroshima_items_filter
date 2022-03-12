a7afa144b0a5095d5c325ce23b155569da2df195
Adrian <adrian.adam.zaleski@gmail.com>
2022-03-12 13:09:33 +0100

    Adds first randomizer function. It has 1 random value that is used in query set of the ranged table.
    In index file - there is display of the difficulty as first tests.

--- neuro/items/urls.py
+++ neuro/items/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'items'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('randomizer', views.randomizer, name='randomizer'),
+
 ]
--- neuro/items/views.py
+++ neuro/items/views.py
@@ -1,7 +1,24 @@
-from django.http import HttpResponse
+import random
 
 # Create your views here.
+from django.shortcuts import render
+from django.views import generic
+
+
+from .models import Ranged, Difficulty
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Neuroshima items index.")
+    difficulty = Difficulty.objects.all()
+    context = {"difficulty": difficulty}
+    return render(request, 'items/index.html', context)
+
+
+def randomizer(request):
+    rand_num = random.randrange(0, 100)
+
+    ranged = Ranged.objects.filter(availability__gte=rand_num)
+    context = {'ranged': ranged, 'rand_num': rand_num}
+
+    return render(request, 'items/randomizer_test.html', context)
+
