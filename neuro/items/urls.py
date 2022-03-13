13bc2c10a2efd1f661c448bac85822946f100d00
Adrian <adrian.adam.zaleski@gmail.com>
2022-03-13 14:43:00 +0100

    Adds: Ammunition randomizer and detail display
    Rename files for easier search:
    randomizer _xx - html for randomizing views
    detail_xxx - htmls for views

--- neuro/items/urls.py
+++ neuro/items/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 app_name = 'items'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('random_ranged', views.randomized_ranged_weapons, name='random ranged weapons'),
-    path('ranged_detail/<str:id_code>', views.ranged_detail, name='ranged_detail'),
+    path('random/ranged/', views.randomized_ranged_weapons, name='random_ranged_weapons'),
+    path('detail/ranged/<str:id_code>', views.detail_ranged, name='detail_ranged'),
+
+    path('random/ammo/', views.randomized_ammo, name='random_ammo'),
+    path('detail/ammo/<str:id_code>', views.detail_ammo, name='detail_ammo'),
 
 ]
+
--- neuro/items/views.py
+++ neuro/items/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 from django.views import generic
 
 
-from .models import Ranged, Difficulty
+from .models import Ranged, Difficulty, Ammo
+
 
 # As a standoff function - to much recursion occurs:
 # USe as a copy to modify in each view separate.
@@ -41,9 +42,25 @@ def randomized_ranged_weapons(request):
     return render(request, 'items/randomizer_weapon.html', context)
 
 
-def ranged_detail(request, id_code):
+def detail_ranged(request, id_code):
     ranged = Ranged.objects.get(id_code=id_code)
-    print(ranged)
     context = {'ranged': ranged}
-    return render(request, 'items/ranged_detail.html', context)
+    return render(request, 'items/detail_ranged.html', context)
 
+
+def randomized_ammo(request):
+    ammunition = Ammo.objects.all()
+    list_of_ammo = []
+    for ammo in ammunition:
+        rand_num = random.randrange(0, 100)
+        if ammo.availability >= rand_num:
+            list_of_ammo.append(ammo)
+    context = {'list_of_ammo': list_of_ammo}
+
+    return render(request, 'items/randomizer_ammo.html', context)
+
+
+def detail_ammo(request, id_code):
+    ammo = Ammo.objects.get(id_code=id_code)
+    context = {'ammo': ammo}
+    return render(request, 'items/detail_ammo.html', context)
