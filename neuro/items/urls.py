a9f75754c52589c19f0652b68444fa4f8e3c7cab
Adrian <adrian.adam.zaleski@gmail.com>
2022-03-19 14:03:58 +0100

    Adds concept of the navbar to the app.
    Still work to do with correct display and usage of static css files.

--- neuro/items/urls.py
+++ neuro/items/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'items'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('random/ranged/', views.randomized_ranged_weapons, name='random_ranged_weapons'),
-    path('detail/ranged/<str:id_code>', views.detail_ranged, name='detail_ranged'),
 
-    path('random/ammo/', views.randomized_ammo, name='random_ammo'),
-    path('detail/ammo/<str:id_code>', views.detail_ammo, name='detail_ammo'),
+    path('ranged/', views.ranged_all, name='all_ranged'),
+    path('ranged/random/', views.randomized_ranged_weapons, name='random_ranged'),
+    path('ranged/detail/<str:id_code>', views.detail_ranged, name='detail_ranged'),
+
+    path('ammo/', views.ammo_all, name='all_ammo'),
+    path('ammo/random/', views.randomized_ammo, name='random_ammo'),
+    path('ammo/detail/<str:id_code>', views.detail_ammo, name='detail_ammo'),
 
 ]
 
