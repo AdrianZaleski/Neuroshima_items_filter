c03e0da05a5532a77c1ee4ae0459ecad12f02d17
Adrian <adrian.adam.zaleski@gmail.com>
2022-05-17 16:00:07 +0200

    script repair - now works good only with pages reload

--- neuro/static/js/script.js
+++ neuro/static/js/script.js
@@ -1,10 +1,28 @@
-console.log("It is working");
+let ikona = localStorage.getItem("navbat_list");
 
-const list = document.querySelectorAll(".navbar_list");
-
-function activeLink() {
-    list.forEach(item => item.classList.remove("active"));
-    this.classList.add("active");
+if (ikona == null) {
+    active("home");
+} else {
+    active(ikona);
 }
 
-list.forEach(item => item.addEventListener("click", activeLink));
+let navbar_list = document.getElementsByClassName("navbar_list");
+
+for (var i = 0; navbar_list.length > i; i++) {
+    navbar_list[i].addEventListener("click", function () {
+        let mode = this.dataset.mode;
+        console.log(this.dataset);
+        console.log("Option clicked: ", mode);
+        setNavSelector(mode);
+    });
+}
+
+function setNavSelector(mode) {
+    console.log("Klikłem", mode);
+    active(mode);
+}
+
+function active(mode) {
+    document.getElementById(mode).classList.add("active");
+    localStorage.setItem("navbat_list", mode);
+}
